61d84b40ac08780b24d687cad41d4da7179cc230
Yohann76 <yohanndurand76@gmail.com>
2021-09-06 15:27:07 +0200

    [Form] add form for get credit

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\CreditType;
 use App\Form\KeywordSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,10 +17,44 @@ class PaymentController extends AbstractController
     /**
      * @Route("/pay/", name="app_pay")
      */
-    public function indexListPrice(): Response
+    public function indexListPrice(Request $request, PhpParser $phpParser): Response
     {
 
+        $form = $this->createForm(CreditType::class);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $credit = $form->getData()['credit']; 
+
+            $coeff = 0 ; 
+
+            if ( $credit < 1000 ) {
+                $price = $credit * 0.17 ; 
+            }
+
+            if ( $credit > 2000 ) {
+                $price = $credit * 0.15 ; 
+            }
+
+            if ( $credit > 4000 ) {
+                $price = $credit * 0.14 ; 
+            }
+
+            if ( $credit > 10000 ) {
+                $price = $credit * 0.10 ; 
+            }
+            
+            return $this->render('payment/pay.html.twig', [
+            'credit' => $credit,
+            'price' => $price,
+        ]);
+        }
+
+
         return $this->render('home/payment.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
